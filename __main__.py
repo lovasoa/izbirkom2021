9e730906f0208ba11a3f63025c1e6c19c296d8ae
Ophir LOJKINE <contact@ophir.dev>
2021-09-20 00:17:24 +0200

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -27,7 +27,7 @@ async def main():
             .parentElement
             .setAttribute(
                 "style", 
-                "position: fixed; top: 0; left: 0; z-index: 999; width: 100%; background: white;"
+                "position: fixed; top: 0; left: 0; z-index: 999; width: 100%; background: white; height: 100%"
             )
     ''')
     await page.screenshot({'path': img, 'fullPage': 'true'})
